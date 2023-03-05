ec45b3ed11040528a1b078dc36ec470719a9025f
Макс <makcmakc321@mail.ru>
2023-03-05 21:01:41 +0300

    UI Fix

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 import './styles/index.scss'
 
-// const search = document.querySelector('.search')
-// const searchInput = document.querySelector('.search-field__input')
-// const searchDrop = document.querySelector('.search-drop')
-
-// searchInput.addEventListener('focus', () => {
-//   searchDrop.classList.add('is-focus')
-//   search.classList.add('is-active')
-// })
-
-// searchInput.addEventListener('blur', () => {
-//   searchDrop.classList.remove('is-focus')
-//   search.classList.remove('is-active')
-// })
-
-
-// const settingOption = document.querySelector('.settings-option')
 
 const searchResultList = document.querySelector('.search-result__list')
-const searchResultChildrten = Array.from(searchResultList.children)
+const searchResultChildrten = [...searchResultList.children]
 
 let itemsQuantityInList = 5
 let resultListHeight = []
 searchResultChildrten.slice(0, itemsQuantityInList).forEach(el => resultListHeight.push(el.clientHeight))
 searchResultList.style.height = resultListHeight.reduce((a, b) => a + b)  + 'px'
 
-// console.log('HEIGHT OF RESULT List', listHeight.reduce((a, b) => a + b))
-// console.log(el.getBoundingClientRect().height, el.offsetHeight, el.clientHeight)
+const search = document.querySelector('.search')
+const searchInput = document.querySelector('.search-field__input')
+const searchDrop = document.querySelector('.search-drop')
+
+searchInput.addEventListener('focus', () => {
+  searchDrop.classList.add('is-focus')
+  search.classList.add('is-active')
+})
+
+searchInput.addEventListener('blur', () => {
+  searchDrop.classList.remove('is-focus')
+  search.classList.remove('is-active')
+})
+
+
+const mock = [
+  { id: 0, firstName: 'Yo', lastName: 'Asakura', img: '', status: 'Active' }
+]
